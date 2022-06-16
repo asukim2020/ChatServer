f2c0377d828e7d273be231f10dbed3e689c540cc
DESKTOP-T1VCKU6\gusrn <asukim2020@gmail.com>
2022-06-16 13:51:18 +0900

    first commit

---
+++ chatserver/src/test/java/com/asusoft/chatserver/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asusoft.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
